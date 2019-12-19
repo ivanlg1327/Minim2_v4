de7e2ecfea26a41e0d59714ef70371dcdcc1395f
Ivan <ivanl.g.27@hotmail.com>
2019-12-19 13:36:16 +0100

    Second com

--- app/src/main/java/com/example/minim2_v4/ListActivity.java
+++ app/src/main/java/com/example/minim2_v4/ListActivity.java
@@ -2,11 +2,14 @@ package com.example.minim2_v4;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 //import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import androidx.recyclerview.widget.RecyclerView;
-//import android.support.v7.widget.LinearLayoutManager;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 ///import android.support.v7.widget.RecyclerView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import java.util.List;
 
@@ -21,7 +24,7 @@ public class ListActivity extends MainActivity{
     TextView textViewNombre;
     ImageView  imageViewMuseo;
     public List<Element> data;
-
+    private LinearLayout linear;
     public Recycler recycler;
     public RecyclerView recyclerView;
     ProgressDialog progressDialog;
@@ -29,6 +32,92 @@ public class ListActivity extends MainActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        recycler = new Recycler(this);
+        recyclerView.setAdapter(recycler);
+        recyclerView.setHasFixedSize(true);
+
+
+        textViewNombre = findViewById(R.id.nombremuseo);
+        imageViewMuseo = findViewById(R.id.imagenmuseo);
+
+
+
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Cargando museos...");
+        progressDialog.setMessage("Espere a que el servidor responda");
+        progressDialog.setCancelable(false);
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.show();
+
+
+        myMuseoApi = MuseoApi.retrofit.create(MuseoApi.class);
+
+        getData();
+
+
+
+    }
+    private void getData(){
+
+        Call<Museums> elementCall = myMuseoApi.getData(1,11);
+
+        elementCall.enqueue(new Callback<Museums>() {
+            @Override
+            public void onResponse(Call<Museums> call, Response<Museums> response) {
+                if(response.isSuccessful()){
+                    Museums museo = response.body();
+
+                    data = museo.getElements();
+                    recycler.rellenarLista(data);
+                    progressDialog.hide();
+
+                }else{
+                    progressDialog.hide();
+                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ListActivity.this);
+
+                    alertDialogBuilder
+                            .setTitle("Error")
+                            .setMessage(response.message())
+                            .setCancelable(false)
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    finish();
+                                }
+                            });
+
+                    AlertDialog alertDialog = alertDialogBuilder.create();
+                    alertDialog.show();
+
+
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<Museums> call, Throwable t) {
+
+                progressDialog.hide();
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ListActivity.this);
+
+                alertDialogBuilder
+                        .setTitle("Error")
+                        .setMessage(t.getMessage())
+                        .setCancelable(false)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        });
+
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+
+            }
+        });
 
 
     }
